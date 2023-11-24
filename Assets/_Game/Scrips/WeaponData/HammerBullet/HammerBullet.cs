72bfbd888333c849e623f147e20c28963ba071c8
lequangdien <93376348+lequangdien@users.noreply.github.com>
2023-11-24 20:38:31 +0700

    24_11_lan2

--- Assets/_Game/Scrips/WeaponData/HammerBullet/HammerBullet.cs
+++ Assets/_Game/Scrips/WeaponData/HammerBullet/HammerBullet.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class HammerBullet : Bullet
 {
-    private float speed = -1000f;
+    private float speedBullet = -1000f;
 
     void Update()
     {
-        transform.Rotate(Vector3.forward, Time.deltaTime * speed);
+        transform.Rotate(Vector3.forward, Time.deltaTime * speedBullet);
     }
 }
